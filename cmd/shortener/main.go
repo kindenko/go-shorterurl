aae42e0d7095436a13561cc2dfcdea84caa3e1a8
Егор Сумихин <bbkegor@bk.ru>
2023-06-03 20:47:08 +0300

    init commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"math/rand"
+	"net/http"
+)
+
+var urls map[string]string
+
+func mainPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		id := randstring()
+		urls[id] = string(b)
+
+		resp := "http://localhost:8080/" + id
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+
+		w.Write([]byte(resp))
+	} else if r.Method == http.MethodGet {
+		id := r.URL.Path[1:]
+		url, ok := urls[id]
+		if !ok {
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+		w.Write([]byte("Location: " + url))
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	}
+}
+
+func main() {
+	urls = make(map[string]string)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", mainPost)
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func randstring() string {
+	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+
+}

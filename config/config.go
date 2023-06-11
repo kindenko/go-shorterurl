612c0ffe10d7ebda9394919823b329a7952a8849
Егор Сумихин <bbkegor@bk.ru>
2023-06-11 20:45:55 +0300

    initial iter4

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"flag"
+	"os"
+)
+
+var SetConfig struct {
+	Host      string
+	ResultURL string
+}
+
+func init() {
+
+	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
+		SetConfig.Host = host
+	} else {
+		flag.StringVar(&SetConfig.Host, "a", "localhost:8080", "It's a Host")
+	}
+	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
+		SetConfig.ResultURL = baseURL
+	} else {
+		flag.StringVar(&SetConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
+	}
+
+}

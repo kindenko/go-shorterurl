513c53799972d3291dce3d33554183ec080110e8
Kirill Prokopovich <kiprokop10@mts.ru>
2023-08-23 11:00:56 +0300

    refactors Handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,37 +5,16 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/kindenko/go-shorterurl/config"
 	"github.com/kindenko/go-shorterurl/internal/app/handlers"
-	"github.com/kindenko/go-shorterurl/internal/app/logger"
-	"github.com/kindenko/go-shorterurl/internal/app/zip"
 )
 
 var urls = make(map[string]string)
 
 func main() {
 	conf := config.NewCfg()
+	newHandlers := handlers.NewHandlers(conf, chi.NewRouter())
 
-	newHandlers := handlers.NewHandlers(conf)
-
-	r := chi.NewRouter()
-	r.Use(logger.WithLogging)
-	r.Use(zip.MiddlewareCompressGzip)
-	r.Use(middleware.Compress(5, "text/html",
-		"application/x-gzip",
-		"text/plain",
-		"application/json"))
-
-	r.Route("/", func(r chi.Router) {
-		r.Post("/", newHandlers.PostHandler)
-		r.Post("/api/shorten", newHandlers.PostJSONHandler)
-		r.Get("/{shortUrl}", newHandlers.GetHandler)
-		r.Get("/ping", newHandlers.Ping)
-		r.Post("/api/shorten/batch", newHandlers.Batch)
-	})
-
-	log.Fatal(http.ListenAndServe(conf.Host, r))
-
+	log.Fatal(http.ListenAndServe(conf.Host, newHandlers))
 }
--- internal/app/handlers/initsettings.go
+++ internal/app/handlers/initsettings.go
@@ -1,16 +1,23 @@
 package handlers
 
 import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kindenko/go-shorterurl/config"
+	"github.com/kindenko/go-shorterurl/internal/app/logger"
 	"github.com/kindenko/go-shorterurl/internal/app/storage"
+	"github.com/kindenko/go-shorterurl/internal/app/zip"
 )
 
 type Handlers struct {
 	cfg     *config.AppConfig
 	storage storage.MyStorage
+	mux     *chi.Mux
 }
 
-func NewHandlers(cfg *config.AppConfig) *Handlers {
+func NewHandlers(cfg *config.AppConfig, mux *chi.Mux) *Handlers {
 
 	c := cfg
 	storage := storage.Init(c)
@@ -18,5 +25,28 @@ func NewHandlers(cfg *config.AppConfig) *Handlers {
 	return &Handlers{
 		cfg:     cfg,
 		storage: storage,
+		mux:     mux,
 	}
+}
+
+func (h *Handlers) Init() {
+	r := chi.NewRouter()
+	r.Use(logger.WithLogging)
+	r.Use(zip.MiddlewareCompressGzip)
+	r.Use(middleware.Compress(5, "text/html",
+		"application/x-gzip",
+		"text/plain",
+		"application/json"))
+
+	r.Route("/", func(r chi.Router) {
+		r.Post("/", h.PostHandler)
+		r.Post("/api/shorten", h.PostJSONHandler)
+		r.Get("/{shortUrl}", h.GetHandler)
+		r.Get("/ping", h.Ping)
+		r.Post("/api/shorten/batch", h.Batch)
+	})
+}
+
+func (h *Handlers) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mux.ServeHTTP(w, r)
 }
